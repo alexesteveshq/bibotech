756d07de1f4f4a22de766d09331a2dec21fa567c
Alex Rafael Esteves Balado <alexrafael.esteves@es.gt.com>
2023-01-17 17:57:09 +0100

    [Alex] modulo para exportar datos

---
+++ odoo_data_export/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Data Export Module',
+    'category': 'Accounting',
+    'version': '15.0.1.0.0',
+    'author': 'Alex Esteves',
+    'maintainer': 'Grant Thornton S.L.P.',
+    'license': 'AGPL-3',
+    'summary': 'Data Export Module',
+    'description': """
+        Data Export Module
+    """,
+    'depends': [
+        'account',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ odoo_data_export/models/data_export.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class DataExport(models.Model):
+    _name = 'data.export'
+    _description = 'Data Export'
